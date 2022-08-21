5664878fbaf6b051833bb84e306d107313898bd9
Ankit Raj <ankitraj@No-internet.local>
2022-08-21 13:36:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(
+    "mongodb+srv://ankit:ankit123@cluster0.jtulkq2.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(PORT))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
